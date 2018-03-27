101a5f19300030b93a4133ff7c5293bb099dca9b
szwiec <kuba.szwiec@gmail.com>
2018-03-27 21:30:50 +0100

    step 12

---
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherDao.java
@@ -0,0 +1,19 @@
+package com.example.android.sunshine.data.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.Date;
+
+@Dao
+public interface WeatherDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void bulkInsert(WeatherEntry... weather);
+
+    @Query("SELECT * FROM weather WHERE date = :date")
+    LiveData<WeatherEntry> getWeatherByDate(Date date);
+}
